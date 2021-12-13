53fb1702c76860efdd0f88e9c64429f675fbb08d
Cristian Cabarcas <cridejecato@gmail.com>
2021-12-13 00:49:23 -0500

    Afirmaciones posteriores a la ejecución

--- src/test/java/com/sofkau/reactive/stepverifier/ServicioTest.java
+++ src/test/java/com/sofkau/reactive/stepverifier/ServicioTest.java
@@ -41,20 +41,33 @@ public class ServicioTest {
     }
     @Test
     void testTodosFiltro() {
-        Flux<String> source = servicio.buscarTodosFiltro();
-        StepVerifier
-            .create(source)
-            .expectNextCount(4)
-            .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException &&
-                    throwable.getMessage().equals("Mensaje de Error")
-            );
+        Flux<Integer> source = Flux.<Integer>create(emitter -> {
+            emitter.next(1);
+            emitter.next(2);
+            emitter.next(3);
+            emitter.complete();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            emitter.next(4);
+        }).filter(number -> number % 2 == 0);
+
+        StepVerifier.create(source)
+            .expectNext(2)
+            .expectComplete()
+            .verifyThenAssertThat()
+            .hasDropped(4)
+            .tookLessThan(Duration.ofMillis(1045));
+
 
         /* Publicadores Basados en Tiempo
         Por ejemplo, suponga que en nuestra aplicación de la vida real, tenemos un retraso de un día entre eventos.
         Ahora, obviamente, no queremos que nuestras pruebas se ejecuten durante todoun día para verificar el
         comportamiento esperado con tal retraso. El constructor StepVerifier.withVirtualTime está diseñado para
         evitar pruebas de larga duración.
-        
+
         StepVerifier
           .withVirtualTime(() -> Flux.interval(Duration.ofSeconds(1)).take(2))
           .expectSubscription()
